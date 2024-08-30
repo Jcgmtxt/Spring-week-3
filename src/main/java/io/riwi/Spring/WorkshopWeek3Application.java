ff37dfffbd1d76adf07464aa475df1e06f0cf4f4
JCGM <juliancamilo0210@gmail.com>
2024-08-30 12:04:59 -0500

    add project structure

---
+++ src/main/java/io/riwi/Spring/WorkshopWeek3Application.java
@@ -0,0 +1,13 @@
+package io.riwi.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkshopWeek3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkshopWeek3Application.class, args);
+	}
+
+}
---
+++ src/test/java/io/riwi/Spring/WorkshopWeek3ApplicationTests.java
@@ -0,0 +1,13 @@
+package io.riwi.Spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopWeek3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
